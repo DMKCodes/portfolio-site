73db9aff7653e9fb77d96aa976b45ba0ced42757
Doug <dmkcodes@gmail.com>
2024-01-17 11:14:09 -0500

    update projects

--- src/components/pages/projects/project-card/PROJECTS.js
+++ src/components/pages/projects/project-card/PROJECTS.js
@@ -1,27 +1,34 @@
 export const PROJECTS = [
     {
         id: 1,
+        name: 'Biodiversity365',
+        stack: ['MongoDB', 'Express', 'React', 'Node.js', 'JWT Auth', 'Redux', 'RTK API', 'Heroku', 'Recharts', 'React Scrollama'],
+        description: 'A biodiversity learning platform with original educational material, stories, and interactive data visualization.',
+        url: 'https://biodiversity365.com/'
+    },
+    {
+        id: 2,
         name: 'Pokébuild',
         stack: ['React', 'Redux', 'Bootstrap', 'Fetch'],
         description: 'A web app utilizing PokéAPI to build and save teams of Pokémon.',
         url: 'https://github.com/DMKCodes/pokebuild'
     },
     {
-        id: 2,
+        id: 3,
         name: 'MERN Boiler',
         stack: ['MongoDB', 'Express', 'React', 'Node.js', 'JWT Auth', 'Redux', 'RTK API'],
         description: 'A robust MERN boilerplate with RESTful API for rapid development of secure, full stack applications.',
         url: 'https://github.com/DMKCodes/mern-boilerplate'
     },
     {
-        id: 3,
+        id: 4,
         name: 'Campsite',
         stack: ['React', 'React Native', 'Redux', 'Bootstrap'],
         description: 'A demo web and mobile app to research, reserve, and review campsites.',
         url: 'https://github.com/DMKCodes/nucampsite-react'
     },
     {
-        id: 4,
+        id: 5,
         name: 'The Lair',
         stack: ['Python', 'OOP', 'Procedural Gen', 'RPG'],
         description: 'A nostalgic, 1980s-style text RPG with roguelike elements and procedural generation.',
